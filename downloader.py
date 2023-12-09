a15afd4af64b42b2a09bcf109d4e30ae32d37745
lucabln05 <luca.bellon@icloud.com>
2023-12-09 16:48:50 +0100

    added user feedback

--- downloader.py
+++ downloader.py
@@ -5,55 +5,67 @@ import music_tag
 
 
 def download_playlist(path, playlist_url, resolution, format='mp4'):
-    
-    #check if playlist or single video
-    if any(x in playlist_url for x in ['list=', 'playlist']):
-        p = Playlist(playlist_url)
-        print(path)
-        print('Number of videos in playlist: %s' % len(p.video_urls))
-        for video in p.videos:
-            #only audio
+    try:
+        #check if playlist or single video
+        if any(x in playlist_url for x in ['list=', 'playlist']):
+            p = Playlist(playlist_url)
+            print(path)
+            print('Number of videos in playlist: %s' % len(p.video_urls))
+            for video in p.videos:
+                #only audio
+                if format == 'mp3':
+                    video.streams.filter(only_audio=True).first().download(path)
+                else:
+                    if resolution == 'High Res':
+                        #download video in highest resolution
+                        video.streams.get_highest_resolution().download(path)
+                    else:
+                        #download video in lowest resolution
+                        video.streams.get_lowest_resolution().download(path)
+        else:
+            #download single video
+            yt = YouTube(playlist_url)
             if format == 'mp3':
-                video.streams.filter(only_audio=True).first().download(path)
+                yt.streams.filter(only_audio=True).first().download(path)
             else:
                 if resolution == 'High Res':
-                    #download video in highest resolution
-                    video.streams.get_highest_resolution().download(path)
+                    yt.streams.get_highest_resolution().download(path)
                 else:
-                    #download video in lowest resolution
-                    video.streams.get_lowest_resolution().download(path)
-    else:
-        #download single video
-        yt = YouTube(playlist_url)
-        if format == 'mp3':
-            yt.streams.filter(only_audio=True).first().download(path)
-        else:
-            if resolution == 'High Res':
-                yt.streams.get_highest_resolution().download(path)
-            else:
-                yt.streams.get_lowest_resolution().download(path)
+                    yt.streams.get_lowest_resolution().download(path)
+        return True
+    except:
+        return False
+    
 def convert_mp3(path, quality='320k'):
-    # convert every mp4 file to mp3
-    for file in os.listdir(path):
-        if file.endswith(".mp4"):
-            file = os.path.join(path, file)
-            filename = os.path.splitext(file)[0]
-            if quality == '320k':
-                # convert to 320k mp3
-                ffmpeg.input(file).output(f"{filename}.mp3", acodec='libmp3lame', ac=2, ar='44100', ab='320k').run()
-            else:
-                # convert to 128k mp3
-                ffmpeg.input(file).output(f"{filename}.mp3", acodec='libmp3lame', ac=2, ar='44100', ab='128k').run()
-            os.remove(file)
+    try:
+        # convert every mp4 file to mp3
+        for file in os.listdir(path):
+            if file.endswith(".mp4"):
+                file = os.path.join(path, file)
+                filename = os.path.splitext(file)[0]
+                if quality == '320k':
+                    # convert to 320k mp3
+                    ffmpeg.input(file).output(f"{filename}.mp3", acodec='libmp3lame', ac=2, ar='44100', ab='320k').run()
+                else:
+                    # convert to 128k mp3
+                    ffmpeg.input(file).output(f"{filename}.mp3", acodec='libmp3lame', ac=2, ar='44100', ab='128k').run()
+                os.remove(file)
+        return True
+    except:
+        return False
             
 def tag_mp3(path, artist, album, genre):
     #add tags to mp3 files
-    for file in os.listdir(path):
-        if file.endswith(".mp3"):
-            # add artist and album tags
-            file = os.path.join(path, file)
-            tag = music_tag.load_file(file)
-            tag['artist'] = artist
-            tag['album'] = album
-            tag['genre'] = genre
-            tag.save()
+    try:
+        for file in os.listdir(path):
+            if file.endswith(".mp3"):
+                # add artist and album tags
+                file = os.path.join(path, file)
+                tag = music_tag.load_file(file)
+                tag['artist'] = artist
+                tag['album'] = album
+                tag['genre'] = genre
+                tag.save()
+        return True
+    except:
+        return False
--- gui.py
+++ gui.py
@@ -2,6 +2,7 @@
 import customtkinter
 from tkinter import filedialog
 from downloader import *
+import time
 
 #initialisierung
 global folder
@@ -42,37 +43,53 @@ class App(customtkinter.CTk):
             folder = filedialog.askdirectory()
 
         def download(): 
+            feedback.configure(text='Downloading...')
             if folder == '':
                 change_folder()
-            download_playlist(folder, entry.get(), res.get(), mp3_var.get())
+            if download_playlist(folder, entry.get(), res.get(), mp3_var.get()):
+                feedback.configure(text='Download finished')
+            else:
+                feedback.configure(text='Download failed - check internet connection')
+                return
             if mp3_var.get() == 'mp3':
-                convert_mp3(folder, quality.get())
+                feedback.configure(text='Converting to mp3...')
+                if convert_mp3(folder, quality.get()):
+                    feedback.configure(text='Conversion finished')
+                else:
+                    feedback.configure(text='Conversion failed - check if ffmpeg is installed')
+                    return
             if tag_var.get() == 'on':
-                tag_mp3(folder, artist_entry.get(), album_entry.get(), genre_entry.get())
+                feedback.configure(text='Tagging mp3...')
+                if tag_mp3(folder, artist_entry.get(), album_entry.get(), genre_entry.get()):
+                    feedback.configure(text='Tagging finished')
+                else:
+                    feedback.configure(text='Tagging failed')
+                    return
 
 
 
         self.title("MusicToIPOD - Youtube Downloader")
         self.geometry("500x400")
+        self.resizable(False, False)
         self.grid_columnconfigure(0, weight=1)
 
+        # first row youtube link and download button
         entry = customtkinter.CTkEntry(self, placeholder_text="Playlist URL")
         entry.grid(row=0, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
-
         download_button = customtkinter.CTkButton(self, text='Download', command=download)
         download_button.grid(row=0, column=3, padx=10, pady=10)
 
+        # second row mp3 checkbox and folder button
         mp3_var = customtkinter.StringVar(value='mp4')
         mp3_checkbox = customtkinter.CTkCheckBox(self, text="MP3", command=mp3_check, variable=mp3_var, onvalue='mp3', offvalue='mp4')
         mp3_checkbox.grid(row=1, column=0, padx=20, pady=(0, 10), sticky="w")
-
         tag_var = customtkinter.StringVar(value='off')
         tag_checkbox = customtkinter.CTkCheckBox(self, text="Tag MP3", command=tag_check, variable=tag_var, onvalue='on', offvalue='off', state='disabled')
         tag_checkbox.grid(row=1, column=1, padx=20, pady=(0, 10), sticky="w")
-
         folder_button = customtkinter.CTkButton(self, text='Folder', command=change_folder)
         folder_button.grid(row=1, column=3, padx=10, pady=10)
 
+        # third row resolution and quality and tag entries
         artist_entry = customtkinter.CTkEntry(self, placeholder_text="Artist")
         artist_entry.grid(row=3, column=1, padx=10, pady=10, sticky="ew")
         album_entry = customtkinter.CTkEntry(self, placeholder_text="Album")
@@ -91,7 +108,12 @@ class App(customtkinter.CTk):
         res.grid(row=3, column=0, padx=10, pady=10, sticky="ew")
         res.set("Low Res")
         res.configure(state='normal')
-                
+
+
+        # user feedback
+        feedback = customtkinter.CTkLabel(self, text='')
+        feedback.grid(row=3, column=3, padx=20, pady=(10), sticky="w")
+
 
 app = App()
 app.mainloop()
